7f5cfa6de27f06cff23f8c9c640c911fdb96c3c1
Praneeth <praneethayla@gmail.com>
2024-02-18 16:37:48 +0530

    balance component

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Balance({ value }) {
+	const [balance, setBalance] = useState();
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:3000/api/v1/account/balance", {
+				headers: {
+					Authorization: "Bearer " + localStorage.getItem("token"),
+				},
+			})
+			.then((res) => {
+				setBalance(res.data.balance);
+			});
+	}, []);
+
 	return (
 		<div className="flex ">
 			<div className="text-lg font-bold">Your Balance</div>
-			<div className="ml-4 text-lg font-semibold">Rs {value}</div>
+			<div className="ml-4 text-lg font-semibold">
+				Rs {balance ? balance.toFixed(2) : "Loading..."}
+			</div>
 		</div>
 	);
 }
